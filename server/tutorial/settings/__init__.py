e9634280eab80f1f5488ad649e1ee818746a6462
hvag-ab <54468623+hvag-ab@users.noreply.github.com>
2021-04-26 14:03:06 +0800

    Update __init__.py

--- server/tutorial/settings/__init__.py
+++ server/tutorial/settings/__init__.py
@@ -1,9 +1,12 @@
 import os
+import platform
 """
 export django_ENV= prod  也可以添加docker 一般默认开发环境
 """
 
-if os.getenv('django_ENV') == 'prod':
+system = platform.system().lower() != 'linux'
+
+if os.getenv('DJANGO_ENV') == 'dev' or system:
     from .prod import *
 else:
     from .dev import *
