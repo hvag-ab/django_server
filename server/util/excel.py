4636b3f277ca89f7d527d47ddc78836dfadd2d3e
hvag-ab <54468623+hvag-ab@users.noreply.github.com>
2021-09-07 10:06:13 +0800

    Update excel.py

--- server/util/excel.py
+++ server/util/excel.py
@@ -1,4 +1,3 @@
-from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from openpyxl import Workbook
 import openpyxl
@@ -8,7 +7,7 @@ from openpyxl.utils import get_column_letter
 from openpyxl.styles import Side, Border, colors, PatternFill, Alignment, Font
 import csv
 from django.http import StreamingHttpResponse
-from typing import List, Union
+from typing import List, Union, Optional
 
 
 
@@ -184,5 +183,84 @@ class ModelToCSV:
                 datas.append(writer.writerow(list(data.values())))
         response = StreamingHttpResponse(datas, content_type="text/csv")
         response['Content-Disposition'] = f'attachment; filename="{self.filename}.csv"'
+        response['Access-Control-Expose-Headers'] = 'Content-Disposition'
+        return response
+
+    
+import re, stat, mimetypes, os
+from pathlib import Path
+from django.utils.http import http_date
+from django.views.static import was_modified_since
+from django.http import FileResponse, HttpRequest, HttpResponseNotModified, Http404
+
+
+# 支持大文件的断点续传（暂停/继续下载）
+class FileDownload:
+
+    def __init__(self, folder: str, filename: str, request: Optional[HttpRequest] = None,
+                 file_name: Optional[str] = None):
+        """
+        :param folder: 下载文件所在的文件夹
+        :param filename: 下载文件名 必须带上文件后缀名 例如a.zip  a.jpg
+        :param request: request对象
+        :param file_name: 保存文件名 默认等于 下载文件名
+        """
+        self.request = request
+        self.path = Path(folder) / filename
+        if not self.path.exists():
+            raise Http404
+        if file_name is None:
+            self.file_name = filename
+        self.stat = self.path.stat()
+
+    @property
+    def export_big(self):
+
+        # 判断下载过程中文件是否被修改过
+        if not was_modified_since(self.request.META.get('HTTP_IF_MODIFIED_SINCE'),
+                                  self.stat.st_mtime, self.stat.st_size):
+            return HttpResponseNotModified()
+
+        # 获取文件的content_type
+        content_type, encoding = mimetypes.guess_type(self.path)
+        content_type = content_type or 'application/octet-stream'
+
+        # 计算读取文件的起始位置
+        start_bytes = re.search(r'bytes=(\d+)-', self.request.META.get('HTTP_RANGE', ''), re.S)
+        start_bytes = int(start_bytes.group(1)) if start_bytes else 0
+
+        # 打开文件并移动下标到起始位置，客户端点击继续下载时，从上次断开的点继续读取
+        the_file = self.path.open('rb')
+        the_file.seek(start_bytes, os.SEEK_SET)
+
+        # status=200表示下载开始，status=206表示下载暂停后继续，为了兼容火狐浏览器而区分两种状态
+        # 关于django的response对象，参考：https://www.cnblogs.com/scolia/p/5635546.html
+        # 关于response的状态码，参考：https://www.cnblogs.com/DeasonGuan/articles/Hanami.html
+        # FileResponse默认block_size = 4096，因此迭代器每次读取4KB数据
+        response = FileResponse(the_file, content_type=content_type, status=206 if start_bytes > 0 else 200,
+                                filename=self.file_name)
+
+        # 'Last-Modified'表示文件修改时间，与'HTTP_IF_MODIFIED_SINCE'对应使用，参考：https://www.jianshu.com/p/b4ecca41bbff
+        response['Last-Modified'] = http_date(self.stat.st_mtime)
+
+        # 这里'Content-Length'表示剩余待传输的文件字节长度
+        if stat.S_ISREG(self.stat.st_mode):
+            response['Content-Length'] = self.stat.st_size - start_bytes
+        if encoding:
+            response['Content-Encoding'] = encoding
+
+        # 'Content-Range'的'/'之前描述响应覆盖的文件字节范围，起始下标为0，'/'之后描述整个文件长度，与'HTTP_RANGE'对应使用
+        # 参考：http://liqwei.com/network/protocol/2011/886.shtml
+        response['Content-Range'] = 'bytes %s-%s/%s' % (start_bytes, self.stat.st_size - 1, self.stat.st_size)
+
+        # 'Cache-Control'控制浏览器缓存行为，此处禁止浏览器缓存，参考：https://blog.csdn.net/cominglately/article/details/77685214
+        response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+        response['Access-Control-Expose-Headers'] = 'Content-Disposition'
+        return response
+
+    @property
+    def export(self):
+        file = self.path.open('rb')
+        response = FileResponse(file, filename=self.file_name)
         response['Access-Control-Expose-Headers'] = 'Content-Disposition'
         return response
