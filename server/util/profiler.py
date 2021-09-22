2ac66aa40e4abf51d3d44b43d26639a496676234
hvag-ab <54468623+hvag-ab@users.noreply.github.com>
2021-09-22 17:48:54 +0800

    Update profiler.py

--- server/util/profiler.py
+++ server/util/profiler.py
@@ -7,24 +7,18 @@ except ImportError:
     pass
 
 
-def profiler(use=True):
-    def deco_func(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            if use:
-                lp = LineProfiler()
-                lp_wrapper = lp(func)
-                result = lp_wrapper(*args, **kwargs)
-                fio = StringIO()
-                lp.print_stats(stream=fio, stripzeros=True)
-                print(fio.getvalue())
-            else:
-                result = func(*args, **kwargs)
-            return result
+def profiler(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        lp = LineProfiler()
+        lp_wrapper = lp(func)
+        result = lp_wrapper(*args, **kwargs)
+        fio = StringIO()
+        lp.print_stats(stream=fio, stripzeros=True)
+        print(fio.getvalue())
+        return result
 
-        return wrapper
-
-    return deco_func
+    return wrapper
     
 """
 使用：
