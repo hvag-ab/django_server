4cb76e91630e34bd2cb55c6f01762e2e7dbd5510
hvag-ab <54468623+hvag-ab@users.noreply.github.com>
2021-10-14 17:08:46 +0800

    Update zip.py

--- server/util/zip.py
+++ server/util/zip.py
@@ -1,36 +1,57 @@
 import zipfile
-import os
 import pathlib
+from typing import Any, Callable, Optional
+from io import BytesIO
 
 
-def zipDir(dirpath, outfolder: str = None, file_name='x', exclude_file: list = None):
+def zip(dirpath=None, files_paths=None, outfolder: Optional[str] = None, file_name: str = 'x',
+        exclude_file: Optional[list] = None):
     """
     压缩指定文件夹
     :param file_name: 文件名称
+    :param files_paths: 指定压缩文件路径
     :param dirpath: 目标文件夹路径
     :param outfolder: 压缩文件保存文件夹路径 默认是在目标文件夹同一个父文件夹中
     :param exclude_file list 排除不需要加入压缩的文件名字
     :return: 无
     """
-    if not outfolder:
+    if exclude_file is None:
+        exclude_file = []
+    if outfolder:
+        outfolder = pathlib.Path(outfolder)
+    elif dirpath and not outfolder:
         p = pathlib.Path(dirpath)
         outfolder = p.parent
     else:
-        outfolder = pathlib.Path(outfolder)
+        raise ValueError(f'dirpath and outfolder 参数不能同时为空')
+
     file_path = str(outfolder / f'{file_name}.zip')
     zip = zipfile.ZipFile(file_path, "w", zipfile.ZIP_DEFLATED)
 
-    for path, dirnames, filenames in os.walk(dirpath):
-        # 去掉目标根路径，只对目标文件夹下边的文件及文件夹进行压缩
-        fpath = path.replace(dirpath, '')
-
-        if exclude_file:
-            filenames = list(set(filenames) - set(exclude_file))
-
-        for filename in filenames:
-            print(filename)
-            zip.write(os.path.join(path, filename), os.path.join(fpath, filename))
+    if dirpath:
+        p = pathlib.Path(dirpath)
+        for d in p.iterdir():
+            name = d.name
+            if name not in exclude_file:
+                zip.write(d)
+    elif files_paths:
+        for pi in files_paths:
+            zip.write(pi)
+    else:
+        raise ValueError('dirpath and files_paths 参数不能同时为空')
     zip.close()
+    return file_path
 
 
+# 多个excel直接在内存中压缩 方便直接传给前端
+def zip_bytes(list_: list, func: Callable[[Any], bytes], suffix:str="xlsx") -> bytes:
+    buffer = BytesIO()
+    zfile = zipfile.ZipFile(buffer, 'w', zipfile.ZIP_DEFLATED, allowZip64=False)
 
+    for i, obj in enumerate(list_):
+        bytes_obj = func(obj)
+        zfile.writestr(f"{i}.{suffix}", bytes_obj)
+    zfile.close()
+    buffer.seek(0)
+    zipped_file = buffer.getvalue()
+    return zipped_file
