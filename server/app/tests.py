3a268e48943e4f633c7db6a7edf3cc93fabade25
hvag-ab <54468623+hvag-ab@users.noreply.github.com>
2021-12-30 14:51:44 +0800

    Update tests.py

--- server/app/tests.py
+++ server/app/tests.py
@@ -2,24 +2,53 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
+from django.core.files import File 
+from django.core.files.uploadedfile import SimpleUploadedFile 
 
 class AccountTests(APITestCase):
     # 测试开始前
     # def setUp(self) -> None:
     #     #self.client.login(username='hvag', password='secret') 登录
     #     self.client.credentials(HTTP_AUTHORIZATION='Token ' + 'xcvdfafda') # token 认证
-
+    
+    # 传递json
     def test_create_account(self):
         """
         Ensure we can create a new account object.
         """
         url = reverse('app:api',kwargs={'version':'v1'})
         data = {'username': 'abc888','password':'123','confirmpassword':'123'}
+        
+        # 默认情况下，format可用的格式是 'multipart' 和 'json' 。为了与 Django 现有的 RequestFactory 兼容，默认格式是 'multipart'。
         response = self.client.post(url, data, format='json')
+        print(response.json())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 2)
+        self.assertEqual(User.objects.get(name='abc888').name, 'abc888')
+        
+        # 传递file
+    def test_upload(self):
+        """
+        Ensure we can create a new account object.
+        """
+        url = reverse('app:api',kwargs={'version':'v1'})
+        file = File(open('media/testfiles/vid.mp4', 'rb'))
+        filename = "hvag"
+        uploaded_file = SimpleUploadedFile(filename, file.read(), content_type='multipart/form-data') 
+        data = {'id': 4,'file':uploaded_file}
+        response = self.client.post(url, data, format='multipart')
         print(response.data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(User.objects.count(), 2)
         self.assertEqual(User.objects.get(name='abc888').name, 'abc888')
+        
+    def test_get(self):
+        url = reverse('app:api',kwargs={'version':'v1'})
+        response = self.client.get(url, {'name': self.name})
+        self.assertEqual(response.status_code, 200)  # 期望的Http相应码为200
+        data = response.json()
+        self.assertEqual(data['msg'], 'Hello Django I am a test Case')  # 期望的msg返回结果为'Hello Django I am a test Case'
+
 
 
     # 测试结束执行
