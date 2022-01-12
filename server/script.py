6ee265a5142d986c35af4c0ce0e8bde8b1f5f40b
hvag-ab <54468623+hvag-ab@users.noreply.github.com>
2022-01-12 14:26:57 +0800

    Update script.py

--- server/script.py
+++ server/script.py
@@ -1,7 +1,11 @@
 # 通过脚本调用django中的函数 变量
 
-import os, django
+import os, sys, django
+from pathlib import Path
 
+DJANGO_DIR = Path(__file__).resolve().parent.parent
+
+sys.path.insert(0, str(DJANGO_DIR))# 否则会报错ModuleNotFoundError
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings.dev")
 django.setup()
 
